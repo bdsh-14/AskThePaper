715bcb806f3ea7ed54334c4de3c0c21cc0cc3ca0
Bidisha Biswas <bidisha.biswas@ibotta.com>
2025-12-05 14:52:20 -0700

    Initial Commit

---
+++ AskThePaper/AskThePaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskThePaperApp.swift
+//  AskThePaper
+//
+//  Created by Bidisha Biswas on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AskThePaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

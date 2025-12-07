64d1ed09d04e10a107e80c5b64119a0db937e94e
Bidisha Biswas <bdsh.14@gmail.com>
2025-12-06 19:15:29 -0700

    Loading States

--- AskThePaper/ContentView.swift
+++ AskThePaper/ContentView.swift
@@ -5,12 +5,51 @@
 //  Created by Bidisha Biswas on 12/5/25.
 //
 
+import PDFKit
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ContentView: View {
+	enum LoadState {
+		case waiting, loading, loaded(String)
+	}
+
+	@State private var loadState: LoadState = .waiting
+
     var body: some View {
-        ChatView()
+		switch loadState {
+		case .waiting:
+			DroppedFileView(onDrop: handleDrop)
+		case .loading:
+			VStack {
+				ProgressView()
+				Text("Processing...")
+			}
+		case .loaded(let string):
+			ChatView()
+		}
     }
+
+	private func handleDrop(item: Data, contentType: UTType) -> Bool {
+		loadState = .loading
+
+		let result = switch contentType {
+		case .pdf: handlePdf(item)
+		default: String?.none
+		}
+
+		if let result {
+			loadState = .loaded(result)
+			return true
+		} else {
+			loadState = .waiting
+			return false
+		}
+	}
+
+	func handlePdf(_ data: Data) -> String? {
+		PDFDocument(data: data)?.string
+	}
 }
 
 #Preview {
